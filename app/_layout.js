b1ded91229a261b4fa55480651a7661d756ec868
Kisan Majumdar <kisanmajumdar44@gmail.com>
2024-12-18 12:00:43 +0530

    new beginning

---
+++ app/_layout.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import "../global.css";
+import { Slot } from "expo-router";
+
+const Layout = () => {
+  return <Slot />;
+};
+
+export default Layout;
