7c732f1f450f26d4876257eb3c72cbdb965c8de0
Kisan Majumdar <kisanmajumdar44@gmail.com>
2024-12-21 21:29:46 +0530

    forget password page

--- app/Screens/signIn.js
+++ app/Screens/signIn.js
@@ -8,6 +8,9 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
 } from "react-native";
 import React, { useRef, useState } from "react";
 import {
@@ -34,110 +37,120 @@ export default function SignIn() {
   };
 
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
-      <StatusBar style="light" backgroundColor="red" />
-      <View
-        style={{
-          paddingTop: hp(8),
-          paddingHorizontal: wp(5),
-          flex: 1,
-          gap: 12,
-        }}
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: "white" }}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
       >
-        {/* signIn Image*/}
-        <View style={{ alignItems: "center" }}>
-          <Image
-            style={{ height: hp(30), resizeMode: "contain" }}
-            source={require("../../assets/images/signUP1.jpg")}
-          />
-        </View>
-        <View style={{ gap: 10 }}>
-          <Text
-            style={{ fontSize: hp(4) }}
-            className="font-bold tracking-wider text-center text-neutral-800"
+        <View style={{ flex: 1, backgroundColor: "white" }}>
+          <StatusBar style="light" backgroundColor="red" />
+          <View
+            style={{
+              paddingTop: hp(8),
+              paddingHorizontal: wp(5),
+              flex: 1,
+              gap: 12,
+            }}
           >
-            Sign In
-          </Text>
-          {/*inputs */}
-          <View style={{ gap: hp(2) }}>
-            <View
-              style={{ height: hp(7) }}
-              className="flex-row font-bold gap-4 px-4 bg-neutral-100 items-center rounded-xl"
-            >
-              <Octicons name="mail" size={hp(2.7)} color="gray" />
-              <TextInput
-                onChangeText={(value) => (emailRef.current = value)}
-                style={{ fontSize: hp(2) }}
-                className="flex-1 font-semibold text-neutral-700"
-                placeholder="Email address"
-                placeholderTextColor={"gray"}
+            {/* signIn Image*/}
+            <View style={{ alignItems: "center" }}>
+              <Image
+                style={{ height: hp(30), resizeMode: "contain" }}
+                source={require("../../assets/images/signUP1.jpg")}
               />
             </View>
-            <View className="gap-3">
-              <View
-                style={{ height: hp(7) }}
-                className="flex-row font-bold gap-4 px-4 bg-neutral-100 items-center rounded-xl"
-              >
-                <Octicons name="lock" size={hp(2.7)} color="gray" />
-                <TextInput
-                  onChangeText={(value) => (passwordRef.current = value)}
-                  style={{ fontSize: hp(2) }}
-                  className="flex-1 font-semibold text-neutral-700"
-                  placeholder="Password"
-                  secureTextEntry
-                  placeholderTextColor={"gray"}
-                />
-              </View>
+            <View style={{ gap: 10 }}>
               <Text
-                style={{ fontSize: hp(1.8) }}
-                className="font-semibold text-right text-neutral-500"
+                style={{ fontSize: hp(4) }}
+                className="font-bold tracking-wider text-center text-neutral-800"
               >
-                Forgot Password ?
+                Sign In
               </Text>
-            </View>
-            {/*Submit Button */}
-            <View>
-              {loading ? (
-                <View className="flex-row justify-center">
-                  <ActivityIndicator size={hp(6.5)} />
+              {/*inputs */}
+              <View style={{ gap: hp(2) }}>
+                <View
+                  style={{ height: hp(7) }}
+                  className="flex-row font-bold gap-4 px-4 bg-neutral-100 items-center rounded-xl"
+                >
+                  <Octicons name="mail" size={hp(2.7)} color="gray" />
+                  <TextInput
+                    onChangeText={(value) => (emailRef.current = value)}
+                    style={{ fontSize: hp(2) }}
+                    className="flex-1 font-semibold text-neutral-700"
+                    placeholder="Email address"
+                    placeholderTextColor={"gray"}
+                  />
                 </View>
-              ) : (
-                <TouchableOpacity
-                  onPress={handleLogin}
-                  style={{ backgroundColor: "red", height: hp(6.5) }}
-                  className="justify-center rounded-xl items-center"
-                >
-                  <Text
-                    style={{ fontSize: hp(2.7) }}
-                    className="text-white font-bold tracking-wider text-center"
+                <View className="gap-3">
+                  <View
+                    style={{ height: hp(7) }}
+                    className="flex-row font-bold gap-4 px-4 bg-neutral-100 items-center rounded-xl"
                   >
-                    Sign In
+                    <Octicons name="lock" size={hp(2.7)} color="gray" />
+                    <TextInput
+                      onChangeText={(value) => (passwordRef.current = value)}
+                      style={{ fontSize: hp(2) }}
+                      className="flex-1 font-semibold text-neutral-700"
+                      placeholder="Password"
+                      secureTextEntry
+                      placeholderTextColor={"gray"}
+                    />
+                  </View>
+                  <Text
+                    style={{ fontSize: hp(1.8) }}
+                    className="font-semibold text-right text-neutral-500"
+                  >
+                    Forgot Password ?
                   </Text>
-                </TouchableOpacity>
-              )}
-            </View>
+                </View>
+                {/*Submit Button */}
+                <View>
+                  {loading ? (
+                    <View className="flex-row justify-center">
+                      <ActivityIndicator size={hp(6.5)} />
+                    </View>
+                  ) : (
+                    <TouchableOpacity
+                      onPress={handleLogin}
+                      style={{ backgroundColor: "red", height: hp(6.5) }}
+                      className="justify-center rounded-xl items-center"
+                    >
+                      <Text
+                        style={{ fontSize: hp(2.7) }}
+                        className="text-white font-bold tracking-wider text-center"
+                      >
+                        Sign In
+                      </Text>
+                    </TouchableOpacity>
+                  )}
+                </View>
 
-            {/*SignUp text */}
-            <View className="flex-row justify-center">
-              <Text
-                style={{ fontSize: hp(1.8) }}
-                className="font-semibold text-neutral-500"
-              >
-                Don't have an account?{" "}
-              </Text>
-              <Pressable onPress={() => navigation.navigate("signUp")}>
-                <Text
-                  style={{ fontSize: hp(1.8), color: "red" }}
-                  className="font-semibold"
-                >
-                  Sign Up
-                </Text>
-              </Pressable>
+                {/*SignUp text */}
+                <View className="flex-row justify-center">
+                  <Text
+                    style={{ fontSize: hp(1.8) }}
+                    className="font-semibold text-neutral-500"
+                  >
+                    Don't have an account?{" "}
+                  </Text>
+                  <Pressable onPress={() => navigation.navigate("signUp")}>
+                    <Text
+                      style={{ fontSize: hp(1.8), color: "red" }}
+                      className="font-semibold"
+                    >
+                      Sign Up
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
             </View>
           </View>
         </View>
-      </View>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
--- app/_layout.js
+++ app/_layout.js
@@ -6,19 +6,21 @@ import SignIn from "../app/Screens/signIn";
 import SignUp from "../app/Screens/signUp";
 import AdminDashboard from "./Screens/AdminDashboard";
 import Maps from "./Screens/Maps";
+import ForgetPassword from "./Screens/ForgetPassword";
 
 const Stack = createNativeStackNavigator();
 
 const RootLayout = () => {
   return (
     <Stack.Navigator
-      initialRouteName="maps"
+      initialRouteName="signUp"
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name="signIn" component={SignIn} />
       <Stack.Screen name="signUp" component={SignUp} />
       <Stack.Screen name="admin" component={AdminDashboard} />
       <Stack.Screen name="maps" component={Maps} />
+      <Stack.Screen name="forgetPassword" component={ForgetPassword} />
     </Stack.Navigator>
   );
 };
