61691b7de8dca8ea3ac70fc7efd6bddbd9d3aa4c
Kisan Majumdar <kisanmajumdar44@gmail.com>
2025-01-08 20:14:57 +0530

    index

--- app/index.js
+++ app/index.js
@@ -1,32 +1,32 @@
-// import React, { useEffect } from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import RootLayout from "./_layout";
-// import { View, ActivityIndicator } from "react-native";
-// import * as Notifications from "expo-notifications";
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import RootLayout from "./_layout";
+import { View, ActivityIndicator } from "react-native";
+import * as Notifications from "expo-notifications";
 
-// export default function App() {
-//   useEffect(() => {
-//     // Configure notifications
-//     Notifications.setNotificationHandler({
-//       handleNotification: async () => ({
-//         shouldShowAlert: true,
-//         shouldPlaySound: false,
-//         shouldSetBadge: false,
-//       }),
-//     });
-//   }, []);
+export default function App() {
+  useEffect(() => {
+    // Configure notifications
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+  }, []);
 
-//   if (initializing) {
-//     return (
-//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//         <ActivityIndicator size="large" color="#ff3b30" />
-//       </View>
-//     );
-//   }
+  if (initializing) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#ff3b30" />
+      </View>
+    );
+  }
 
-//   return (
-//     <NavigationContainer>
-//       <RootLayout />
-//     </NavigationContainer>
-//   );
-// }
+  return (
+    <NavigationContainer>
+      <RootLayout />
+    </NavigationContainer>
+  );
+}
