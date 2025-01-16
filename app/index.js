dad15fd1d2c5295229da57f263fefa8be8f3dfef
Kisan Majumdar <kisanmajumdar44@gmail.com>
2025-01-16 11:21:42 +0530

    added onauth in index.js

--- app/index.js
+++ app/index.js
@@ -1,44 +1,44 @@
-// import React, { useEffect } from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import RootLayout from "./_layout";
-// import { View, ActivityIndicator } from "react-native";
-// import * as Notifications from "expo-notifications";
-// import { auth } from "./firebase";
-// import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import RootLayout from "./_layout";
+import { View, ActivityIndicator } from "react-native";
+import * as Notifications from "expo-notifications";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
-// export default function App() {
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (user) => {
-//       if (user) {
-//         // Handle user state changes
-//       }
-//     });
+export default function App() {
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // Handle user state changes
+      }
+    });
 
-//     return () => unsubscribe();
-//   }, []);
+    return () => unsubscribe();
+  }, []);
 
-//   useEffect(() => {
-//     // Configure notifications
-//     Notifications.setNotificationHandler({
-//       handleNotification: async () => ({
-//         shouldShowAlert: true,
-//         shouldPlaySound: false,
-//         shouldSetBadge: false,
-//       }),
-//     });
-//   }, []);
+  useEffect(() => {
+    // Configure notifications
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+  }, []);
 
-//   if (initializing) {
-//     return (
-//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//         <ActivityIndicator size="large" color="#ff3b30" />
-//       </View>
-//     );
-//   }
+  if (initializing) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#ff3b30" />
+      </View>
+    );
+  }
 
-//   return (
-//     <NavigationContainer>
-//       <RootLayout />
-//     </NavigationContainer>
-//   );
-// }
+  return (
+    <NavigationContainer>
+      <RootLayout />
+    </NavigationContainer>
+  );
+}
