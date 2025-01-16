cafb66395070c2b6d62f02b1f3c2ec36a9da817f
Kisan Majumdar <kisanmajumdar44@gmail.com>
2025-01-16 11:21:12 +0530

    error was in firebase.js file has been solved

--- app/firebase.js
+++ app/firebase.js
@@ -1,31 +1,31 @@
-// import { initializeApp, getApps, getApp } from "firebase/app";
-// import {
-//   getAuth,
-//   initializeAuth,
-//   getReactNativePersistence,
-// } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAOYGu7urmHsctySzztIV0bDEEAOP2BJWM",
-//   authDomain: "live-tracker-e9f51.firebaseapp.com",
-//   projectId: "live-tracker-e9f51",
-//   storageBucket: "live-tracker-e9f51.firebasestorage.app",
-//   messagingSenderId: "508336873226",
-//   appId: "1:508336873226:web:115c138ea73a496ded24f3",
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOYGu7urmHsctySzztIV0bDEEAOP2BJWM",
+  authDomain: "live-tracker-e9f51.firebaseapp.com",
+  projectId: "live-tracker-e9f51",
+  storageBucket: "live-tracker-e9f51.firebasestorage.app",
+  messagingSenderId: "508336873226",
+  appId: "1:508336873226:web:115c138ea73a496ded24f3",
+};
 
-// // Initialize Firebase app if it hasn't been initialized already
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase app if it hasn't been initialized already
+const app = initializeApp(firebaseConfig);
 
-// // Initialize Auth
-// export const auth = initializeAuth(app, {
-//   persistence: getReactNativePersistence(AsyncStorage), // Use AsyncStorage persistence
-// });
+// Initialize Auth
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage), // Use AsyncStorage persistence
+});
 
-// // Initialize Firestore
-// export const db = getFirestore(app);
+// Initialize Firestore
+export const db = getFirestore(app);
 
-// export default app;
+export default app;
