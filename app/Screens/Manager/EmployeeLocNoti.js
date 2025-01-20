c0bb3ad948fff9f9ed11cb49f90e7c2c0d05b020
Kisan Majumdar <kisanmajumdar44@gmail.com>
2025-01-20 11:32:33 +0530

    created a new file EmpLocNoti

--- app/Screens/Manager/EmployeeLocNoti.js
+++ app/Screens/Manager/EmployeeLocNoti.js
@@ -1,12 +1,12 @@
-// import { StyleSheet, Text, View } from "react-native";
-// import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-// export default function EmployeeLocNoti() {
-//   return (
-//     <View>
-//       <Text>EmployeeLocNoti</Text>
-//     </View>
-//   );
-// }
+export default function EmployeeLocNoti() {
+  return (
+    <View>
+      <Text>EmployeeLocNoti</Text>
+    </View>
+  );
+}
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({});
